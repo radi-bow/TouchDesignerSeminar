909c3587fb95ccf4678983e8f438ddbb266988da
Daichi Ito <math144377@gmail.com>
2019-09-23 20:28:47 +0900

    add files

---
+++ WithTD/Assets/Scripts/OSCValueSender.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using uOSC;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OSCValueSender : MonoBehaviour
+{
+
+    [SerializeField] uOscClient client;
+    Slider slider;
+
+    private void Start ()
+    {
+        slider = GetComponent<Slider> ();
+        if (!slider) return;
+    }
+
+    public void OnValueChanged ()
+    {
+        client.Send ("/TouchDesigner/val", slider.value);
+    }
+}
